f1c8610093254063449987d499ea28c6b91e28f5
vmrodriguez <victormanuelrodriguez90@gmail.com>
2023-05-22 14:08:48 -0300

    nextjs setup

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,50 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import Auth0Provider from "next-auth/providers/auth0";
+
+const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
+
+if (!process.env.AUTH0_CLIENT_ID || !process.env.AUTH0_CLIENT_SECRET)
+  throw new Error("Failed to initialize AUTH0 authentication");
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+   Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER_BASE_URL
+    })
+  ],
+  pages: {
+    signIn: `/login`,
+    verifyRequest: `/login`,
+    error: "/login", // Error code passed in query string as ?error=
+  },
+  adapter: PrismaAdapter(prisma),
+  cookies: {
+    sessionToken: {
+      name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
+        domain: VERCEL_DEPLOYMENT ? ".vercel.pub" : undefined,
+        secure: VERCEL_DEPLOYMENT,
+      },
+    },
+  },
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+        username: user.username,
+      },
+    }),
+  },
+};
+
+export default NextAuth(authOptions);
